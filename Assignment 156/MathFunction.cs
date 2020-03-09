44c494bfb4f05c47944d55f08a7f996f00cbae77
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-08 17:44:55 -0700

    Add project files.

---
+++ Assignment 156/MathFunction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_156
+{
+    public class MathFunction
+    {
+            public int addition(int num1, int num2)
+            {
+                var Total = num1 + num2;
+                return Total;
+            }
+            public int multiply(int num1, int num2)
+            {
+                var Total = num1 * num2;
+                return Total;
+            }
+            public int subtract(int num1, int num2)
+            {
+                var Total = num1 - num2;
+                return Total;
+            }
+    }
+}
---
+++ Assignment 156/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Assignment_156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int b = 4;
+            int Total;
+            MathFunction functionOne = new MathFunction();
+
+            Total = functionOne.addition(x, b);
+            Console.WriteLine("The sum of " + x + " plus " + b + " equals:" + Total);
+            Total = functionOne.subtract(x, b);
+            Console.WriteLine("The sum of " + x + " minus " + b + " equals:" + Total);
+            Total = functionOne.multiply(x, b);
+            Console.WriteLine("The sum of " + x + " multiplied by " + b + " equals:" + Total);
+            Console.ReadLine();
+        }
+
+    }
+}
